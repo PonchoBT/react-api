07c5399c7c84d43e84db03ff90c5f1f66bd26126
Alfonso BT <ponchobt.dev@gmail.com>
2023-04-19 22:26:51 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+
+function ApiExample() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Lista de tareas:</h1>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ApiExample;
